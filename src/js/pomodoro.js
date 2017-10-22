53cc2490fe2555dd7031134e200151fad0ecad23
Deyan Vladkov <deian.vladkov@gmail.com>
2017-10-22 18:24:26 +0100

    Added simple counter

--- src/js/pomodoro.js
+++ src/js/pomodoro.js
@@ -1,4 +1,4 @@
-"use strict";
+//"use strict";
 
 function clock() {
     var time = (new Date).toTimeString().slice(0, 8);
@@ -6,4 +6,22 @@ function clock() {
     //console.log(time)
 }
 
-var timer = setInterval(clock, 1000);
+//var timer = setInterval(clock, 1000);
+var count = 0;
+function counter() {
+    count += 1;
+    document.getElementById('clock').innerHTML = '<p>' + displayTime(count) + '</p>';
+}
+
+var timer = setInterval(counter, 1000);
+function displayTime(time) {
+    //ar minutes = Math.floor(time / 60);
+    //var seconds = time - minutes * 60;
+    //return minutes + ':' + seconds;
+    var minutes = "0" + Math.floor(time / 60);
+    var seconds = "0" + (time - minutes * 60);
+    return minutes.substr(-2) + ":" + seconds.substr(-2);
+}
+
+
+
