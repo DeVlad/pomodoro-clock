9acb277ccc6d529e9cc2ebb0867c0e5c6fcf6d8e
Deyan Vladkov <deian.vladkov@gmail.com>
2017-10-23 17:08:42 +0100

    Added session duration input boxes

--- src/js/pomodoro.js
+++ src/js/pomodoro.js
@@ -1,27 +1,31 @@
 //"use strict";
 
-function clock() {
-    var time = (new Date).toTimeString().slice(0, 8);
-    document.getElementById('clock').innerHTML = '<p>' + time + '</p>';
-    //console.log(time)
-}
-
-//var timer = setInterval(clock, 1000);
 var count = 0;
-function counter() {
+var duration = 25; //max duration 60 min
+var rest = 5;      //max break 20 min
+var timer;
+
+function counter(d) {
     count += 1;
+    if(count >= duration) {
+        // Stop timer
+        clearInterval(timer);
+    }    
     document.getElementById('clock').innerHTML = '<p>' + displayTime(count) + '</p>';
 }
 
-var timer = setInterval(counter, 1000);
 function displayTime(time) {
-    //ar minutes = Math.floor(time / 60);
-    //var seconds = time - minutes * 60;
-    //return minutes + ':' + seconds;
     var minutes = "0" + Math.floor(time / 60);
     var seconds = "0" + (time - minutes * 60);
     return minutes.substr(-2) + ":" + seconds.substr(-2);
 }
 
-
-
+// Start timer
+function startPomodoro(d) {
+    document.getElementById('start').onclick = 'stopPomodoro()';
+    rest = document.getElementById('session-duration').value;
+    duration = document.getElementById('break-duration').value;
+    console.log('rest ', rest, ' sess ', duration);
+    duration = d;
+    timer = setInterval(counter, 1000);
+}
